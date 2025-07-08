6ab4c9fca66ebd15cf0b9aea90b7811f29da6ee5
scarv <fernando.carvalho@lcsistemas.com.br>
2025-07-08 17:05:54 -0300

    docs: criado rest client http

---
+++ src/db/connection.ts
@@ -0,0 +1,10 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import { env } from '../env.ts';
+import postgres from 'postgres';
+import { schema } from './schema/index.ts';
+
+export const sql = postgres(env.DATABASE_URL)
+export const db = drizzle(sql, {
+    schema,
+    casing: 'snake_case',
+})
